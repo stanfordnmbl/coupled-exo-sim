1e6a6baae92b3d5e84cf120985e885176fd90cff
nickbianco <nbianco@stanford.edu>
2017-08-11 14:14:09 -0700

    Add .gitignore; add main python-doit files

---
+++ dodo.py
@@ -0,0 +1,50 @@
+# Allow using the osimpipeline git submodule.
+import sys
+sys.path.insert(1, 'osimpipeline')
+
+import os
+
+import yaml
+with open('config.yaml') as f:
+    config = yaml.load(f)
+if not 'opensim_home' in config:
+    raise Exception("You must define the field `opensim_home` in config.yaml "
+            "to point to the root of your OpenSim 3.3 installation.")
+sys.path.insert(1, os.path.join(config['opensim_home'], 'sdk', 'python'))
+sys.path.insert(1, 'perimysium')
+
+DOIT_CONFIG = {
+        'verbosity': 2,
+        'default_tasks': None,
+        }
+
+# Settings for plots.
+import matplotlib
+if matplotlib.__version__[0] == '1':
+    raise Exception("Must have matplotlib version 2 to avoid "
+            "incorrect bar plots.")
+import matplotlib.pyplot as plt
+plt.rc('font', family='Helvetica, Arial, sans-serif', size=8)
+plt.rc('errorbar', capsize=1.5)
+plt.rc('lines', markeredgewidth=1)
+plt.rc('legend', fontsize=8)
+
+import osimpipeline as osp
+
+# This line is necessary for registering the tasks with python-doit.
+from osimpipeline.vital_tasks import *
+
+# Custom tasks for this project.
+from tasks import *
+
+study = osp.Study('exotoplogy',
+        'Rajagopal2015_18musc_muscle_names_probed.osim',
+        'Rajagopal2015_reserve_actuators.xml')
+
+# Add tasks for each subject. To prevent this file from being HUGE, we split
+# each subject out into its own file.
+import subject01
+subject01.add_to_study(study)
+
+# Analysis
+# --------
---
+++ tasks.py
@@ -0,0 +1,93 @@
+import os
+
+import pylab as pl
+import pandas as pd
+
+import osimpipeline as osp
+
+class working_directory():
+    """Use this to temporarily run code with some directory as a working
+    directory and to then return to the original working directory::
+
+        with working_directory('<dir>'):
+            pass
+    """
+    def __init__(self, path):
+        self.path = path
+        self.original_working_dir = os.getcwd()
+    def __enter__(self):
+        os.chdir(self.path)
+    def __exit__(self, *exc_info):
+        os.chdir(self.original_working_dir)
+
+
+class TaskCopyMotionCaptureData(osp.TaskCopyMotionCaptureData):
+    REGISTRY = []
+    def __init__(self, subject, 
+            walk100=None, walk125=None, walk150=None, walk175=None,
+            run200=None, run300=None, run400=None, run500=None):
+        regex_replacements = list()
+        for datastr, condname, arg in [
+                ('Walk_100', 'walk1', walk100),
+                ('Walk_125', 'walk2', walk125),
+                ('Walk_150', 'walk3', walk150),
+                ('Walk_175', 'walk4', walk175),
+                ('Run_200', 'run1', run200),
+                ('Run_300', 'run2', run300),
+                ('Run_400', 'run3', run400),
+                ('Run_500', 'run4', run500)]:
+            # Marker trajectories.
+            regex_replacements.append(
+                (
+                    os.path.join(subject.name, 'Data',
+                        '%s %02i.trc' % (datastr, arg[0])).replace('\\','\\\\'),
+                    os.path.join('experiments',
+                        subject.name, condname, 'expdata', 
+                        'marker_trajectories.trc').replace('\\','\\\\')
+                    ))
+            # Ground reaction.
+            regex_replacements.append(
+                (
+                    os.path.join(subject.name, 'Data',
+                        '%s %02i%s.mot' % (datastr, arg[0],arg[1])).replace(
+                            '\\','\\\\'),
+                    os.path.join('experiments', subject.name, condname,
+                        'expdata','ground_reaction_orig.mot').replace(
+                            '\\','\\\\') 
+                    )) 
+        regex_replacements.append((
+                    os.path.join(subject.name, 'Data',
+                        'Static_FJC.trc').replace('\\','\\\\'),
+                    os.path.join('experiments', subject.name, 'static',
+                        'trial01', 'expdata',
+                        'marker_trajectories.trc').replace('\\','\\\\') 
+                    ))
+        super(TaskCopyMotionCaptureData, self).__init__(subject,
+                regex_replacements)
+
+class TaskUpdateGroundReactionColumnLabels(osp.TrialTask):
+    REGISTRY = []
+    def __init__(self, trial):
+        super(TaskUpdateGroundReactionColumnLabels, self).__init__(trial)
+        self.name = trial.id + '_update_grf_column_labels'
+        self.add_action(
+                [os.path.join(trial.expdata_path, 'ground_reaction_orig.mot')],
+                [trial.ground_reaction_fpath],
+                self.dispatch)
+    def dispatch(self, file_dep, target):
+        from perimysium.dataman import storage2numpy, ndarray2storage
+        import re
+        data = storage2numpy(file_dep[0])
+        new_names = list()
+        for name in data.dtype.names:
+            if name == 'time':
+                new_name = name
+            elif name.endswith('_1'):
+                new_name = re.sub('ground_(.*)_(.*)_1', 'ground_\\1_l_\\2',
+                        name)
+            else:
+                new_name = re.sub('ground_(.*)_(.*)', 'ground_\\1_r_\\2',
+                        name)
+            new_names.append(new_name)
+        data.dtype.names = new_names
+        ndarray2storage(data, target[0])
